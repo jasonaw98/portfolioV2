ae49f419303b7aa10ef17cba51f038fae41b2651
jason.aw <jason.ahw@masverse.com.my>
2024-06-07 18:48:43 +0800

    first commit

---
+++ data/index.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About",
+      link: "/",
+    },
+    {
+      name: "Contact",
+      link: "/",
+    },
+  ];
